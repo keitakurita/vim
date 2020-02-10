f45fe206d2c4da7ea5334565b6c133a51f41355d
keitakurita <keita@robustintelligence.com>
2020-02-10 00:19:38 +0000

    Add nvidia-empty

--- link.sh
+++ link.sh
@@ -2,3 +2,4 @@ ln -sf $HOME/.vim/.vimrc $HOME/.vimrc
 ln -sf $HOME/.vim/.tmux.conf $HOME/.tmux.conf
 mkdir -p $HOME/bin
 ln -sf $HOME/.vim/my_utils/clean-branches $HOME/bin/clean-branches
+ln -sf $HOME/.vim/my_utils/nvidia-empty $HOME/bin/nvidia-empty
