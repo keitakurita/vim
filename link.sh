c3bdfae7b2d2313891bcba136384a9e90f9b940c
keitakurita <keita@robustintelligence.com>
2020-02-19 16:04:17 +0000

    Add imgcat

--- link.sh
+++ link.sh
@@ -3,3 +3,4 @@ ln -sf $HOME/.vim/.tmux.conf $HOME/.tmux.conf
 mkdir -p $HOME/bin
 ln -sf $HOME/.vim/my_utils/clean-branches $HOME/bin/clean-branches
 ln -sf $HOME/.vim/my_utils/nvidia-empty $HOME/bin/nvidia-empty
+ln -sf $HOME/.vim/my_utils/imgcat $HOME/bin/imgcat
