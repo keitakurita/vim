f8dec27f520165411e4fa527d96cc5d64ecc2fbd
keitakurita <keita@robustintelligence.com>
2020-02-26 23:42:27 +0000

    Added xtra zshrc configs and pbcp command

--- link.sh
+++ link.sh
@@ -1,6 +1,9 @@
 ln -sf $HOME/.vim/.vimrc $HOME/.vimrc
 ln -sf $HOME/.vim/.tmux.conf $HOME/.tmux.conf
+# custom configs
+ln -sf $HOME/.vim/.zshrc_xtra $HOME/.zshrc_xtra
 mkdir -p $HOME/bin
 ln -sf $HOME/.vim/my_utils/clean-branches $HOME/bin/clean-branches
 ln -sf $HOME/.vim/my_utils/nvidia-empty $HOME/bin/nvidia-empty
 ln -sf $HOME/.vim/my_utils/imgcat $HOME/bin/imgcat
+ln -sf $HOME/.vim/my_utils/pbcp $HOME/bin/pbcp
